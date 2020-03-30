bc9aa1b300a01edf03f58cada0c0e220c30d2b13
= <=>
2020-03-30 10:33:25 -0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,9 @@ module.exports = {
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
+        compress: { 
+          drop_console:true
+        },
         test: /\.js(\?.*)?$/i
       })
     ]

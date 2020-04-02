0326c11ce6b7f039e125eb2a3868b4042b365f8e
= <=>
2020-04-02 07:52:27 -0300

    fix color chat

---
+++ src/views/components/messages/styled.js
@@ -0,0 +1,22 @@
+import  styled from 'styled-components';
+
+
+export const ContainerMessage = styled.div`
+    overflow:auto;
+    padding-top: 20px;
+`
+
+
+
+export const MessageBox = styled.div`
+    background:rgb(36, 37, 38);
+    display: inline-block;
+    width:auto;
+    padding:10px;
+`
+
+
+
+
+
+
--- src/views/components/messagesList/index.js
+++ src/views/components/messagesList/index.js
@@ -1,13 +1,13 @@
 import React, {useRef, useEffect} from 'react';
 import { Row, Col, Container } from 'react-grid';
 import {ContainerMessages} from './styled';
+import Message from '../messages';
 
 
 function MessagesList(props) {
 
     const {messages, height} = props;
     let containerMessagesRef = useRef(null);
-
 
     useEffect(() => {
         scrollToBottom()
@@ -18,27 +18,14 @@ function MessagesList(props) {
         containerMessagesRef && (containerMessagesRef.current.scrollTop = containerMessagesRef.current.scrollHeight);
     }
  
-
   return (
 
 
-
-
-    
-
-    <ContainerMessages ref={containerMessagesRef} height={height}>
-        <Col   sm={12} md={12} style={{paddingTop:'20px'}}>
-            {  messages && messages.map(item =>
-                ((item.user && item.message) &&
-                    <div>
-                        {item.user} : {item.message}
-                    </div>
-                )
-            )}
-    </Col>
-</ContainerMessages>
-
-
+        <ContainerMessages ref={containerMessagesRef} height={height}>
+            <Col   sm={12} md={12}>
+                {messages.map((message, id) =><Message { ...message }  key={ id } /> )}
+            </Col>
+        </ContainerMessages>
 
 
   );
